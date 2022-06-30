023620d7a185baca0b1d91ecb91e4d4fe712cae4
devilarmaan <devilarmaan63@gmail.com>
2022-06-30 22:09:03 +0600

    Add decimal to binary

--- binary-decimal.cpp
+++ binary-decimal.cpp
@@ -6,22 +6,55 @@ using namespace std;
 int main () {
 
     // decimal to binary convertion
+    // int n;
+    // cin >> n;
+
+    // int ans = 0;
+    // int i = 0;
+    // while( n != 0){
+    //     int bit =  n & 1;
+    //     ans = (bit * pow(10, i) ) + ans;
+
+    //     n = n >> 1;
+    //     i++;
+
+    // }
+
+    cout << "Answer is" << ans << endl;
+
+    // binary to decimal
     int n;
     cin >> n;
 
-    int ans = 0;
     int i = 0;
+    int ans = 0;
+
     while( n != 0){
-        int bit = n & 1;
-        ans = (bit * pow(10, i) ) + ans;
+        int digit = n % 10;
 
-        n = n >> 1;
+        if(digit == 1){
+            ans = ans + pow(2, i);
+        }
+        n = n/10;
         i++;
-
     }
-
-    cout << "Answer is" << ans << endl;
+    cout << ans << endl;
+    // another afforoach o binary to decimal
     
+  int n;
+  cout << "Enter a  binary number" << endl;
+  cin >> n;
+  long long int ans = 0;
+  int i = 0;
+  while (n > 0) {
+
+    int bit = n & 1;
+    ans = bit * pow(2, i) + ans;
+    n = n / 10;
+    i++;
+  }
+  cout << ans; 
+
     return 0;
 
 }
