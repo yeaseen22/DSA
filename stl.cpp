351d1189af5882b65e945fcbe9659de5e73e62be
devilarmaan <devilarmaan63@gmail.com>
2022-09-15 23:13:38 +0600

    add Struct file

--- stl.cpp
+++ stl.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
+#include <bits/stdc++.h>
 #include <array>
-#include<vector>
-#include <deque>
-#include <list>
+// #include<vector>
+// #include <deque>
+// #include <list>
 
 
 using namespace std;
 
 int main(){
 
-    int basic[3] = {1,2,3};
-    array<int, 4> a= {1,2,3,4};
-
-    vector<int> n;
-    n.push_back(1);
-    // cout<< n.capacity()<<endl;
-    for(int i:basic){
-        cout << i <<endl;
+    array<int, 5> arr;
+    arr.fill(10);
+    for(int i=0; i < arr.size(); i++){
+        cout << arr.at(i) << ",";
     }
 
-    deque<int> v;
-    v.push_back(1);
-    v.push_front(1);
-    v.erase(v.begin(), v.begin()+1);
+    // vector<int> n;
+    // n.push_back(1);
+    // // cout<< n.capacity()<<endl;
+    // for(int i:basic){
+    //     // cout << i <<endl;
+    // }
+
+    // deque<int> v;
+    // v.push_back(1);
+    // v.push_front(1);
+    // v.erase(v.begin(), v.begin()+1);
     return 0;
 }
---
+++ struct.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+
+ using namespace std;
+ 
+ struct car {
+    string name;
+    string color;
+    int maxSpeed;
+    int model;
+ };
+
+ void fun (car x) {
+    cout << "Name" << x.name << endl;
+    cout << "Color" << x.color << endl;
+    cout << "Model" << x.model << endl;
+    cout << "maxSpeed" << x.maxSpeed << endl;
+ }
+ 
+
+ int main (){
+
+ car c1 = {"BMW", "Red", 250, 2022};
+
+ fun(c1);
+
+
+ return 0;
+}
+
+
+
