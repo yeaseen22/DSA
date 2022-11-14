22507e4d3815e2b3fa3cd8e2d5b808feae4d25e6
devilarmaan <devilarmaan63@gmail.com>
2022-11-14 12:55:10 +0600

    Bubble Sort

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -26,6 +26,34 @@ int binarySearch(int arr[], int size, int key){
     }
 
 
+    // Bubble sort
+    void bubbleSortAdavtive(int *arr, int size)
+{
+    int isSorted;
+    for (int i = 0; i < size - 1; i++) // for passes
+    {
+        isSorted = true;
+        // cout << "pass no " << i + 1 << endl; for visualzing passes
+
+        for (int j = 0; j < size - 1 - i; j++) // for comparison 
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                // swap and decided the isSorted
+                int tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+                isSorted = false;
+            }
+        }
+        if (isSorted) // if sorted
+        {
+            return;
+        }
+    }
+}
+
+
 int main(){
 
     // binary search should be in monotonic function, increasing or dicreasing order
