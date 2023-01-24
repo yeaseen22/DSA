79d5d1e543b9e0bc246b5ff2b0bcfba563f4a442
devilarmaan! <devilarmaan63@gmail.com>
2023-01-24 14:53:59 +0600

    add book allocation

--- bookAllocation.cpp
+++ bookAllocation.cpp
@@ -2,7 +2,66 @@
 
 using namespace std;
 
-int main () {
+bool isPossible(vector<int> arr, int n, int m, int mid)
+{
+    int studentCount = 1;
+    int pageSum = 0;
+    // cout << "checking for mid "<< mid <<endl;
 
-    return 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (pageSum + arr[i] <= mid)
+        {
+            pageSum += arr[i];
+        }
+        else
+        {
+            studentCount++;
+            if (studentCount > m || arr[i] > mid)
+            {
+                return false;
+            }
+            pageSum = arr[i];
+        }
+        if (studentCount > m)
+        {
+            return false;
+        }
+        // cout << " for i " << i << " Student "<< studentCount << " pageSum " << pageSum << endl;
+    }
+    return true;
+}
+
+int main()
+{
+
+    i nt allocateBooks(vector<int> arr, int n, int m)
+    {
+        int s = 0;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+        }
+        int e = sum;
+        int ans = -1;
+        int mid = s + (e - s) / 2;
+
+        while (s <= e)
+        {
+            if (isPossible(arr, n, m, mid))
+            {
+                // cout<<" Mid returned TRUE" << endl;
+                ans = mid;
+                e = mid - 1;
+            }
+            else
+            {
+                s = mid + 1;
+            }
+            mid = s + (e - s) / 2;
+        }
+        return ans;
+    }
 }
