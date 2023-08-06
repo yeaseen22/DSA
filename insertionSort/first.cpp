aa8e475776201715abe2397140f33f38b11de7ed
devilamzad <devilarmaan63@gmail.com>
2023-08-06 17:44:26 +0600

    add some file

--- insertionSort/first.cpp
+++ insertionSort/first.cpp
@@ -7,7 +7,7 @@ void insertionSort(int arr[], int n)
     for (i = 1; i < n; i++)
     {
         key = arr[i];
-        j = i - 1;
+        j = i - 1; 
         while (j >= 0 && arr[j] > key)
         {
             arr[j + 1] = arr[j];
---
+++ quickSort2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+// Function to swap two elements
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+// Partition function
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; // Choosing the last element as the pivot
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+// Quick Sort function
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivotIndex = partition(arr, low, high);
+
+        quickSort(arr, low, pivotIndex - 1); // Sort the elements before the pivot
+        quickSort(arr, pivotIndex + 1, high); // Sort the elements after the pivot
+    }
+}
+
+int main() {
+    int arr[] = {5, 2, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, n - 1);
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
